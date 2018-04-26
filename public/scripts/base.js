97d13e48f6c4cf26b77e372e272343d7b385a72e
Milly <31337438+MillyP1@users.noreply.github.com>
2018-04-25 21:51:01 -0700

    Add files via upload

---
+++ public/scripts/base.js
@@ -0,0 +1,1 @@
+ctx = document.getElementById('canvas').getContext('2d');
---
+++ public/scripts/main.js
@@ -0,0 +1,6 @@
+var can1 = document.getElementByID('can1');
+var context = can1.getContext('2d');
+context.font = '32pt Ariel';
+context.fillStyle = 'Solid White';
+
+context.fillText("Test", 20, 200);
